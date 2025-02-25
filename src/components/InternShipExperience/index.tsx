a40bd2a76a812a5fbfb03777435faa8319a93e52
dundun003 <liangshaojun@papegames.net>
2025-02-25 10:23:53 +0800

    update

--- src/components/InternShipExperience/index.tsx
+++ src/components/InternShipExperience/index.tsx
@@ -13,6 +13,7 @@ interface InternShipType {
   commpanyLogoUrl: string;
   discription: string;
   workContent: string[];
+  workTechContent: string[];
   techStack: string[];
 }
 interface InternShipItemProps {
@@ -23,6 +24,7 @@ interface InternShipItemProps {
   discription: string;
   workContent: string[];
   techStack: string[];
+  workTechContent: string[];
 }
 const mock: InternShipType[] = [
   {
@@ -30,19 +32,19 @@ const mock: InternShipType[] = [
     company: "快手（北京海淀) |  企业应用部",
     job: "前端开发实习生",
     commpanyLogoUrl: kuaishouLogo,
-    discription:'',
+    discription: "",
     workContent: [
-      "独立负责快手财务部门多个ToB系统包括企业权限中台、财务核销平台等后台管理系统，解决了页面跳转逻辑修复，数据可视化开发，增加主题个性化等大大小小的需求数共10余项；",
-      "协助完成快手金融主页、企业员工用车报告的页面开发；协助开发Kim内置应用快手差旅应用，简化了快手员工差旅、酒店账单预定、管理、报销流程；独立从0到1搭建了供应商发票协同系统，完善了与供应商之间补充发票的流程链路；独立从无到有的搭建了铁豆项目，实现了差旅报销金额到虚拟货币铁豆的兑换、购买商品的流程；接触并熟悉了企业级项目从需求评审、业务开发、提测、自测和上线的整个流程；"
+      "独立负责快手财务部门多个ToB系统包括企业权限中台、财务核销平台等后台管理系统",
+      "协助完成快手金融主页、企业员工用车报告的页面开发；协助开发Kim内置应用快手差旅应用，简化了快手员工差旅、酒店账单预定、管理、报销流程；独立从0到1搭建了供应商发票协同系统，完善了与供应商之间补充发票的流程链路；独立从无到有的搭建了铁豆项目（移动端H5），实现了差旅报销金额到虚拟货币铁豆的兑换、购买商品的流程；接触并熟悉了企业级项目从需求评审、业务开发、提测、自测和上线的整个流程；",
       // "深度参与组件库建设，自主设计并封装可复用的通用组件，如支持联动搜索的可编辑下拉框、条件筛选半屏组件等，提升团队研发效率；",
     ],
-    techStack: [
-      "React",
-      "TypeScript",
-      "Antd",
-      "Antd Mobile",
-      "Vite",
+    workTechContent: [
+      "搭建包括表单填写，Excel文件快速导入、导出报表、批量提交等环节的三级审批流；基于不同审批状态渲染不同权限的系统页面、操作权限；",
+      "利用WebWorker+虚拟列表优化海量数据表格背景下的核销系统的交互计算流程，减少页面的卡顿感；",
+      "铁豆项目中实现铁豆兑换本地商品的商品页瀑布流+无限滚动等展示布局效果；基于AntdMobile封装移动端的半屏升起、条件筛选等业务组件；",
+      "基于i18n和Antd个性化配置，实现供应商发票协同系统等的语言国际化，主题个性化；二次封装验证码校验等业务组件；",
     ],
+    techStack: ["React", "TypeScript", "Antd", "Antd Mobile", "Vite"],
   },
 ];
 
@@ -54,6 +56,7 @@ const InternShipItem: React.FC<InternShipItemProps> = ({
   discription,
   workContent,
   techStack,
+  workTechContent,
 }) => {
   return (
     <div>
@@ -65,10 +68,24 @@ const InternShipItem: React.FC<InternShipItemProps> = ({
       </div>
       <div className="discription">
         <div className="detail">
-          <span style={{ fontWeight: "bold", fontSize: "18px" }}>实习产出：</span>
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            实习产出：
+          </span>
           {discription}
         </div>
         {workContent.map((content, index) => (
+          <div key={index} className="discription-line">
+            <div className="circle">{index + 1}</div>
+            {content}
+          </div>
+        ))}
+        <div className="detail">
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            技术产出：
+          </span>
+          {discription}
+        </div>
+        {workTechContent.map((content, index) => (
           <div key={index} className="discription-line">
             <div className="circle">{index + 1}</div>
             {content}
@@ -105,6 +122,7 @@ const InternShip: React.FC = () => {
           commpanyLogoUrl={item.commpanyLogoUrl}
           discription={item.discription}
           workContent={item.workContent}
+          workTechContent={item.workTechContent}
           techStack={item.techStack}
         />
       ))}
