079aa2a6be7a47b5aa7674e44417e80ccd8cfb8c
shabix <2936670268@qq.com>
2025-07-15 19:03:55 +0800

    feat: style

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import BasicInfo from "./components/BasicInfo";
 import InternShip from "./components/InternShipExperience";
 import Project from "./components/ProjectExperience";
 import "./App.scss";
+import "./print.scss";
 import Skills from "./components/Skills";
 import { usePDF } from "react-to-pdf";
 import OpenSource from "./components/openSource";
+import { useEffect } from "react";
 
 const Resume = () => {
   const { toPDF, targetRef } = usePDF({
     filename: "26届前端实习 梁少峻 南邮.pdf",
   });
+
+  useEffect(() => {
+    const viewportMeta = document.createElement('meta');
+    viewportMeta.name = 'viewport';
+    viewportMeta.content = 'width=device-width, initial-scale=1.0';
+    document.head.appendChild(viewportMeta);
+
+    return () => {
+      document.head.removeChild(viewportMeta);
+    };
+  }, []);
 
   return (
     <div className="App">
