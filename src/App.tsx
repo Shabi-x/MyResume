d476c652e115ce6ccdf088938b8491e0cd04214a
shabix <2936670268@qq.com>
2025-07-16 15:50:55 +0800

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,19 @@ import "./App.scss";
 import "./print.scss";
 import Skills from "./components/Skills";
 import OpenSource from "./components/openSource";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Snackbar, Alert, AlertTitle } from "@mui/material";
 import githubLogo from "./logo/github.png";
 
 const Resume = () => {
+  const [open, setOpen] = useState(false);
 
   const exportPDF = () => {
     window.print();
   };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     const viewportMeta = document.createElement("meta");
@@ -36,7 +41,7 @@ const Resume = () => {
       </div>
 
       <div className="github-footer">
-        <a href="https://github.com/Shabi-x/MyResume" target="_blank" rel="noopener noreferrer" className="github-link">
+        <a onClick={handleOpen} className="github-link" style={{ cursor: 'pointer' }}>
           <div className="github-avatar">
             <img src={githubLogo} alt="GitHub" />
           </div>
@@ -46,6 +51,17 @@ const Resume = () => {
           Download as PDF
         </button>
       </div>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert severity="info" variant="filled">
+          抱歉，该简历模板的代码暂未打算开源，敬请期待后续更新！
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
