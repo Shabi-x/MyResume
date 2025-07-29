3f735b0cb7e735b468147ea3bcc78aa70b1997c8
shabix <2936670268@qq.com>
2025-07-29 19:53:46 +0800

    update

--- src/components/ProjectExperience/index.tsx
+++ src/components/ProjectExperience/index.tsx
@@ -9,14 +9,14 @@ const projectList = [
     responseJob: "（前端负责人）",
     projectTime: "2023.07 - 至今",
     projectDescription:
-      "南邮小程序(官方用名“南京邮电大学”)，是青柚工作室为师生提供课表，晨跑，一卡通，成绩查询，场地预约，电费充值等功能而开发的微信小程序，日活用户1W+。",
+      "南邮小程序(官方用名“南京邮电大学”)，是青柚工作室为师生提供课表，晨跑，一卡通，成绩查询，场地预约等功能而开发的微信小程序，日活用户1W+。",
     projectList: [
       "开发业务：校园资讯（利用dangerouslySetInnerHTML实现小程序富文本渲染、附件预览）；针对微信丢失、本硕连读用户账号变更问题搭建用户解绑页，设计使用状态控制动画的思路实现登录过程中复杂的动画效果；利用IntersectionObserver实现校园资讯页列表无限滚动；仿照豆瓣UI重构图书馆模块，实现对于书籍详细信息，图书馆藏，借阅记录的查阅；开发消息盒子通知等模块；",
       "业务代码模块化：将UI层与业务逻辑层解耦，实现数据驱动与UI渲染的分离；实现pendingCacheQueue对于请求做缓存优化，状态管理、接入了redux-logger中间件，实现日志跟踪实现封装；",
       "工程化配置：推动使用pnpm管理，依赖升级，配置代码风格规范；积极拥抱Taro 3.6/4.0更新，不断优化师生日常使用体验；",
       "同时担任企业微信侧南邮小程序客服，根据全校师生反馈问题思考解决方案，及时Oncall；",
     ],
-    techStack: ["React", "TypeScript", "Taro", "Redux-toolkit", "pnpm"],
+    techStack: ["React18", "TypeScript", "Taro4.0", "Redux-toolkit", "pnpm"],
     link: "https://qingyou.njupt.edu.cn/",
   },
   // {
